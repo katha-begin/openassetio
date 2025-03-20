59ba8d364ebb49190b94e14bad289161e7221ea4
David Feltell <david.feltell@foundry.com>
2025-03-20 10:51:57 +0000

    squash: Add CppPluginSystemUIDelegateImplementationFactory
    Add a `CppPluginSystemUIDelegateImplementationFactory` that mirrors the
    implementation of `CppPluginSystemManagerImplementationFactory`. This
    class houses the logic required to load C++ UI delegate plugins.
    Managers must implement their plugin class as a subclass of
    `CppPluginSystemUIDelegatePlugin`, again mirroring the manager plugin
    system. Ultimately, it is not expected that hosts will use this class
    directly, other than constructing it, and will instead make use of
    a `UIDelegateFactory`.
    
    Signed-off-by: David Feltell <david.feltell@foundry.com>

---
+++ src/openassetio-ui/tests/pluginSystem/resources/plugins/StubUIDelegateInterface.hpp
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025 The Foundry Visionmongers Ltd
+#pragma once
+
+#include <openassetio/errors/exceptions.hpp>
+#include <openassetio/ui/managerApi/UIDelegateInterface.hpp>
+
+struct StubUIDelegateInterface : openassetio::ui::managerApi::UIDelegateInterface {
+  // TODO(DF): fill in details when base class is implemented.
+
+  [[nodiscard]] openassetio::Identifier identifier() const override {
+    return "org.openassetio.test.pluginSystem."
+           "resources." OPENASSETIO_CORE_PLUGINSYSTEM_TEST_PLUGIN_ID_SUFFIX;
+  }
+
+  // Deliberately throw an exception, for use in checking RTTI.
+  openassetio::InfoDictionary info() override {
+    throw openassetio::errors::NotImplementedException{"Stub doesn't support info"};
+  }
+};
